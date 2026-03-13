32ab4ecfefc230308dad868565c4a24d787abee8
jedick <j3ffdick@gmail.com>
2026-03-13 14:15:43 +0800

    Add CT2

---
+++ CT2/sw.js
@@ -0,0 +1,71 @@
+// The version of the cache.
+const VERSION = "v1";
+
+// The name of the cache
+const CACHE_NAME = `period-tracker-${VERSION}`;
+
+// The static resources that the app needs to function.
+const APP_STATIC_RESOURCES = [
+  // The dot-slash takes care of index.html
+  "./",
+  "./app.js",
+  "./style.css",
+  // If we don't include the manifest in the cache,
+  // it gets a 404 after reloading the page and the app becomes uninstallable
+  "./cycletracker.json",
+  // The app icon should be listed here for installation on iOS and Android
+  "./icons/tire.svg",
+  // This is a higher-resolution icon inherited from upstream that currently isn't used
+  "./icons/wheel.svg",
+];
+
+// On install, cache the static resources
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })()
+  );
+});
+
+// delete old caches on activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+      await clients.claim();
+    })()
+  );
+});
+
+// On fetch, intercept server requests
+// and respond with cached responses instead of going to network
+self.addEventListener("fetch", (event) => {
+  // As a single page app, direct app to always go to cached home page.
+  if (event.request.mode === "navigate") {
+    event.respondWith(caches.match("./"));
+    return;
+  }
+
+  // For all other requests, go to the cache first, and then the network.
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      const cachedResponse = await cache.match(event.request);
+      if (cachedResponse) {
+        // Return the cached response if it's available.
+        return cachedResponse;
+      }
+      // If resource isn't in the cache, return a 404.
+      return new Response(null, { status: 404 });
+    })()
+  );
+});
